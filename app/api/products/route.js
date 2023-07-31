16140f5dc199a1d3c9f188d49c266a70bf16b91a
franki <mahdipratama29@gmail.com>
2023-07-31 16:57:23 +0700

    connected to mongoDB server

---
+++ app/api/products/route.js
@@ -0,0 +1,9 @@
+import { connectToDB, initMongoose } from '@/lib/mongoose';
+import Product from '@/models/Product';
+import { NextResponse } from 'next/server';
+
+export async function GET(request, response) {
+  // await initMongoose();
+  await connectToDB();
+  return NextResponse.json(await Product.find().exec());
+}
