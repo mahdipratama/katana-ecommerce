015e6a4ae243e3b829f5cd2c8d4684e0eda44e49
franki <mahdipratama29@gmail.com>
2023-08-03 15:42:48 +0700

    define reusable button component

---
+++ components/Button.jsx
@@ -0,0 +1,32 @@
+import classNames from 'classnames';
+
+const Button = ({ children, primary, secondary, shadow, ...rest }) => {
+  const classes = classNames(
+    rest.className,
+    'flex items-center px-3 py-1.5 rounded-[3px]',
+    {
+      'bg-primary text-white': primary && !shadow,
+      'bg-secondary text-white px-[32px] py-[12px] uppercase tracking-widest':
+        secondary && !shadow,
+      'bg-accent2 text-black': shadow,
+    }
+  );
+
+  return (
+    <button {...rest} className={classes}>
+      {children}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  checkVariationValue: ({ primary, secondary, shadow }) => {
+    const count = Number(!!primary) + Number(!!secondary) + Number(!!shadow);
+
+    if (count > 1) {
+      return new Error('Please select once');
+    }
+  },
+};
+
+export default Button;
