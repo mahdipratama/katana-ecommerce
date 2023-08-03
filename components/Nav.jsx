a2898231827b9f3b0c65d13ac21fcc8b17919e3b
franki <mahdipratama29@gmail.com>
2023-08-03 14:33:25 +0700

    add navbar style mobile screen size

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,9 +1,14 @@
+'use client';
+
 import Link from 'next/link';
 import Image from 'next/image';
+import { useState } from 'react';
 
 function Nav() {
+  const [activeMenu, setActiveMenu] = useState(false);
+
   return (
-    <nav className="flex items-center justify-between my-10">
+    <nav className="layout flex items-center justify-between">
       <div className="logo">
         <Link href={'/'}>
           <Image
@@ -11,23 +16,53 @@ function Nav() {
             alt="logo"
             width={180}
             height={180}
+            priority={true}
           />
         </Link>
       </div>
-      <ul className="text-[18px] font-light flex gap-11">
-        <Link href={'/'}>
-          <li>Home</li>
-        </Link>
-        <Link href={'/anime'}>
-          <li>Anime katana</li>
-        </Link>
-        <Link href={'/custom'}>
-          <li>Custom katana</li>
-        </Link>
-        <Link href={'/tanto'}>
-          <li>Tanto</li>
-        </Link>
-      </ul>
+
+      <div
+        onClick={() => setActiveMenu(prev => !prev)}
+        className="hamburger z-10 ">
+        <svg
+          className={`ham hamRotate ham8 ${activeMenu && 'active'}`}
+          viewBox="0 0 100 100"
+          width="60">
+          <path
+            className="line top"
+            d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20"
+          />
+          <path className="line middle" d="m 30,50 h 40" />
+          <path
+            className="line bottom"
+            d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"
+          />
+        </svg>
+      </div>
+
+      <div
+        className={`
+        wrapper_menu ${activeMenu && 'active'} bg-secondary 
+        sm:bg-main sm:static sm:ml-10`}>
+        <ul
+          onClick={() => setActiveMenu(false)}
+          className="absolute top-[20%] left-[20%] gap-8 text-[18px] 
+          flex flex-col text-white font-semibold
+          sm:static sm:text-[16px] sm:font-light sm:flex-row sm:gap-5 md:gap-8 lg:gap-11 sm:text-headingColor">
+          <Link href={'/'}>
+            <li>Home</li>
+          </Link>
+          <Link href={'/anime'}>
+            <li>Anime katana</li>
+          </Link>
+          <Link href={'/custom'}>
+            <li>Custom katana</li>
+          </Link>
+          <Link href={'/tanto'}>
+            <li>Tanto</li>
+          </Link>
+        </ul>
+      </div>
     </nav>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,21 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        headingText: 'Merriweather, serif',
+        plainText: 'Rubik, sans-serif',
+      },
+      colors: {
+        main: '#ffff',
+        primary: '#40444d',
+        secondary: '#d81f27',
+        headingColor: '#181919',
+        paragraphColor: '#1a1e1eb3',
+        accent1: '#1a1e1e80',
+        accent2: '#d81f271a',
+        accent3: '#fefefecc',
       },
     },
   },
   plugins: [],
-}
+};
