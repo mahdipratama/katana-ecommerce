bd59db028e614b2dcc329ed6d9bf2d3bcc4513fa
franki <mahdipratama29@gmail.com>
2023-08-03 12:09:16 +0700

    add starter styles and style navbar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
 function Nav() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex items-center justify-between my-10">
+      <div className="logo">
+        <Link href={'/'}>
+          <Image
+            src="/assets/images/logo.png"
+            alt="logo"
+            width={180}
+            height={180}
+          />
+        </Link>
+      </div>
+      <ul className="text-[18px] font-light flex gap-11">
+        <Link href={'/'}>
+          <li>Home</li>
+        </Link>
+        <Link href={'/anime'}>
+          <li>Anime katana</li>
+        </Link>
+        <Link href={'/custom'}>
+          <li>Custom katana</li>
+        </Link>
+        <Link href={'/tanto'}>
+          <li>Tanto</li>
+        </Link>
+      </ul>
+    </nav>
+  );
 }
 
 export default Nav;
