3398c670ca7c07704be59303bb839beec73eee93
franki <mahdipratama29@gmail.com>
2023-08-04 09:10:00 +0700

    add hero section style for desktop screen size

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,20 +5,30 @@ import Button from './Button';
 
 function Hero() {
   return (
-    <section className="layout">
-      <div>
-        <img src="/assets/images/hero.png" alt="hero section image" />
+    <section
+      className="layout 
+      lg:flex lg:flex-row-reverse 
+    ">
+      <div className="flex-1">
+        <img
+          className="mx-auto"
+          src="/assets/images/hero.png"
+          alt="hero section image"
+        />
       </div>
-      <div className="mt-10 flex flex-col items-center text-center">
-        <h1 className="font-headingText text-[38px] font-bold leading-[1.1] mb-6">
+      <div className="mt-10 flex-1 flex flex-col items-center text-center lg:text-left lg:items-start">
+        <h1
+          className="font-headingText text-[38px] font-bold leading-[1.1] mb-6 
+          md:w-[70%] lg:text-[50px] lg:w-[80%] 
+        ">
           Own a Piece of Tradition with Our Katana Collection
         </h1>
-        <p className="text-sm leading-[1.5] text-paragraphColor w-[80%] mb-9">
+        <p className="text-sm leading-[1.5] text-paragraphColor w-[80%] mb-9 md:w-[60%] lg:w-[80%] lg:text-base lg:ml-1">
           Awaken your inner warrior and embrace the philosophy of the Katana.
           With razor-sharp precision and a legacy that echoes through centuries,
           these swords embody the essence of discipline and strength.
         </p>
-        <Link href={'/'}>
+        <Link href={'/'} className="lg:ml-1">
           <Button secondary>shop now</Button>
         </Link>
       </div>
