de948a469ec725ef050b73e466e49d2a79f5b5c4
franki <mahdipratama29@gmail.com>
2023-08-09 19:38:34 +0700

    add style desktop screen size for single product page

--- app/product/page.jsx
+++ app/product/page.jsx
@@ -34,60 +34,56 @@ function Product() {
       {isLoading ? (
         <p>Loading...</p>
       ) : (
-        <div className="flex flex-col items-center">
-          <div className="mb-4">
+        <div className="flex flex-col items-center lg:flex-row">
+          <div className="mb-4 lg:!flex-1">
             <ProductImages images={product.pictures} />
           </div>
 
-          <div className="mb-4 self-start">
-            <h3 className="text-[28px] text-primary font-semibold">
-              {product.name}
-            </h3>
-            <div className="flex items-center">
-              <span className="mr-3 text-secondary text-[25px] font-semibold">
-                $ {product.prices.discPrice}
-              </span>
-              <span className="text-accent1 line-through mb-[-2px]">
-                $ {product.prices.oldPrice}
-              </span>
+          <div className="lg:ml-8 lg:!flex-1 max-w-[603px]">
+            <div className="mb-4 self-start w-full sm:self-auto">
+              <h3 className="text-[28px] text-primary font-semibold">
+                {product.name}
+              </h3>
+              <div className="flex items-center">
+                <span className="mr-3 text-secondary text-[25px] font-semibold">
+                  $ {product.prices.discPrice}
+                </span>
+                <span className="text-accent1 line-through mb-[-2px]">
+                  $ {product.prices.oldPrice}
+                </span>
+              </div>
             </div>
-          </div>
-
-          <hr className="mb-4 w-[603px]" />
-
-          <div className="max-w-[603px]">
-            <p className="text-paragraphColor leading-[1.5]">
-              <span className="font-semibold">Accessories:</span>{' '}
-              {`${product.description.accessories}`} <br />
-              <span className="font-semibold">Material: </span>
-              {`${product.description.material}`}
-              <br />
-              <span className="font-semibold">Sheat Material:</span>
-              {`${product.description.sheatMaterial}`} <br />
-              <span className="font-semibold">Process:</span>
-              {` ${product.description.process}`} <br />
-              <span className="font-semibold">Width:</span>
-              {` ${product.description.width}`} <br />
-              <span className="font-semibold">Length:</span>
-              {` ${product.description.length}`} <br />
-              <span className="font-semibold">Thickness:</span>
-              {` ${product.description.thickness}`}
-            </p>
-          </div>
-
-          <hr className="my-4 w-[603px]" />
-
-          <div className="w-[200px] h-[40px]">
-            <Button primary>
-              Add to cart{' '}
-              <span className="ml-2">
-                <img
-                  className="w-[20px] h-[20px]"
-                  src="/assets/icons/cart.png"
-                  alt="cart icon"
-                />
-              </span>
-            </Button>
+            <div className="w-full border-y-2 py-4">
+              <p className="text-paragraphColor leading-[1.5]">
+                <span className="font-semibold">Accessories:</span>{' '}
+                {`${product.description.accessories}`} <br />
+                <span className="font-semibold">Material: </span>
+                {`${product.description.material}`}
+                <br />
+                <span className="font-semibold">Sheat Material:</span>
+                {`${product.description.sheatMaterial}`} <br />
+                <span className="font-semibold">Process:</span>
+                {` ${product.description.process}`} <br />
+                <span className="font-semibold">Width:</span>
+                {` ${product.description.width}`} <br />
+                <span className="font-semibold">Length:</span>
+                {` ${product.description.length}`} <br />
+                <span className="font-semibold">Thickness:</span>
+                {` ${product.description.thickness}`}
+              </p>
+            </div>
+            <div className="w-[200px] h-[40px] mt-4">
+              <Button primary>
+                Add to cart{' '}
+                <span className="ml-2">
+                  <img
+                    className="w-[20px] h-[20px]"
+                    src="/assets/icons/cart.png"
+                    alt="cart icon"
+                  />
+                </span>
+              </Button>
+            </div>
           </div>
         </div>
       )}
--- components/ProductImages.jsx
+++ components/ProductImages.jsx
@@ -6,8 +6,8 @@ function ProductImages({ images }) {
 
   return (
     <>
-      <div>
-        <img src={activeImage} alt="product image" />
+      <div className="h-[300px] max-w-[600px] sm:h-[370px]">
+        <img src={activeImage} alt="product image" className="h-full w-full" />
       </div>
 
       <div className="flex gap-2 my-2 max-w-[603px]">
@@ -15,8 +15,8 @@ function ProductImages({ images }) {
           <div
             className={`max-h-[80px] border p-0.5 cursor-pointer rounded-[3px]  ${
               image === activeImage
-                ? 'border-2 border-gray-300'
-                : 'border-transparent'
+                ? 'border-2 border-gray-300 '
+                : 'border-transparent opacity-60'
             }`}
             key={image}
             onClick={() => setActiveImage(image)}>
