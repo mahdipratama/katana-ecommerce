6e79074f7b011a444e25e1c4aaf7759ed8408ed0
franki <mahdipratama29@gmail.com>
2023-08-11 16:28:35 +0700

    update end-point api to get specific products by IDs

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -2,7 +2,25 @@ import { connectToDB } from '@/lib/mongoose';
 import Product from '@/models/Product';
 import { NextResponse } from 'next/server';
 
-export async function GET(request, response) {
+export async function findAllProducts() {
   await connectToDB();
-  return NextResponse.json(await Product.find().exec());
+
+  return Product.find().exec();
+}
+
+export async function GET(request) {
+  await connectToDB();
+
+  const ids = request.nextUrl.searchParams.get('ids');
+
+  if (ids) {
+    const idsArray = ids.split(',');
+
+    // Fetch products by specific IDs
+    const products = await Product.find({ _id: { $in: idsArray } }).exec();
+    return NextResponse.json(products);
+  } else {
+    // Fetch all products
+    return NextResponse.json(await findAllProducts());
+  }
 }
