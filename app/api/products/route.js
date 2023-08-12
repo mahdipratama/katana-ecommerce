b144df8606ef75656a7dc7c4fff7ac9844024adf
franki <mahdipratama29@gmail.com>
2023-08-12 13:23:05 +0700

    refactor products end-point

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -2,15 +2,9 @@ import { connectToDB } from '@/lib/mongoose';
 import Product from '@/models/Product';
 import { NextResponse } from 'next/server';
 
-export async function findAllProducts() {
-  await connectToDB();
-
-  return Product.find().exec();
-}
-
 export async function GET(request) {
   await connectToDB();
 
   // Fetch all products
-  return NextResponse.json(await findAllProducts());
+  return NextResponse.json(await Product.find().exec());
 }
